c0564f416d2d5512db73c93fd8c3e0d0b05945ba
D3lux3 <38981163+D3lux3@users.noreply.github.com>
2020-09-18 15:38:24 +0300

    Modifications to Djikstra class.

--- src/main/java/algoritmit/Djikstra.java
+++ src/main/java/algoritmit/Djikstra.java
@@ -1,4 +1,11 @@
 package algoritmit;
 
+import Tietorakenteet.Lista;
+import logiikka.Solmu;
+import java.util.PriorityQueue;
+
 public class Djikstra {
+
+    private Lista<Solmu> reitti;
+    private PriorityQueue<>
 }
