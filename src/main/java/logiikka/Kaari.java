b3157540ea1b621cf0828243eeac4050f26ce6d7
D3lux3 <38981163+D3lux3@users.noreply.github.com>
2020-09-18 15:13:29 +0300

    Added Kaari

---
+++ src/main/java/logiikka/Kaari.java
@@ -0,0 +1,15 @@
+package logiikka;
+
+
+public class Kaari {
+
+    public Solmu alku;
+    public Solmu loppu;
+    public double paino;
+
+    public Kaari(Solmu alku, Solmu loppu, double paino) {
+        this.alku = alku;
+        this.loppu = loppu;
+        this.paino = paino;
+    }
+}
